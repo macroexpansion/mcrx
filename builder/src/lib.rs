637e6dde92f4de4bc9ab3f1f70c75cbdc0c15bef
macroexpansion <trasuadev@gmail.com>
2023-10-21 20:20:08 +0700

    builder pass test1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{parse, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast: DeriveInput = parse(input).unwrap();
 
-    unimplemented!()
+    TokenStream::new()
 }
