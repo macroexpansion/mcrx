c5f1ca739aa717f1e53ef787eea8eeecb3821149
macroexpansion <trasuadev@gmail.com>
2023-10-22 00:32:05 +0700

    impl setters for builder struct

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -21,6 +21,28 @@ fn impl_builder(ast: &DeriveInput) -> TokenStream {
             current_dir: Option<String>,
         }
 
+        impl #builder_struct_ident {
+            pub fn executable(&mut self, value: String) -> &mut Self {
+                self.executable = Some(value);
+                self
+            }
+
+            pub fn args(&mut self, value: Vec<String>) -> &mut Self {
+                self.args = Some(value);
+                self
+            }
+
+            pub fn env(&mut self, value: Vec<String>) -> &mut Self {
+                self.env = Some(value);
+                self
+            }
+
+            pub fn current_dir(&mut self, value: String) -> &mut Self {
+                self.current_dir = Some(value);
+                self
+            }
+        }
+
         impl #ident {
             pub fn builder() -> #builder_struct_ident {
                 #builder_struct_ident {
